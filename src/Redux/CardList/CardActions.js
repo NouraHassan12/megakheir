4d5cd1d2ea04e4969abe35480821ac4cdb11afd5
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-10-02 07:11:45 +0200

    updatesessjkklllklllougu

--- src/Redux/CardList/CardActions.js
+++ src/Redux/CardList/CardActions.js
@@ -36,8 +36,15 @@ export const fetchCards = () => {
     //     'Content-Type': 'application/json',
     //   },
     // };
+    if (window.location.protocol.indexOf('https') == 0){
+      var el = document.createElement('meta')
+      el.setAttribute('http-equiv', 'Content-Security-Policy')
+      el.setAttribute('content', 'upgrade-insecure-requests')
+      document.head.append(el)
+    }
     axios
       .get("http://62.68.253.183:8000/ngos/" ,  )
+    
       .then((res) => {
         console.log(res.data.data,"cardss");
         const cards = res.data.data;
@@ -50,12 +57,7 @@ export const fetchCards = () => {
         dispatch(fetchCardsFailure(errorMsg));
       });
 
-      // if (window.location.protocol.indexOf('https') == 0){
-      //   var el = document.createElement('meta')
-      //   el.setAttribute('http-equiv', 'Content-Security-Policy')
-      //   el.setAttribute('content', 'upgrade-insecure-requests')
-      //   document.head.append(el)
-      // }
+  
   };
 };
 
