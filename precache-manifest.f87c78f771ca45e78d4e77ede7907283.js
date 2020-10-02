c1239f5eae6ec37b53d63866b4093a5fa22f5c06
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-10-02 03:19:36 +0200

    Updates

---
+++ precache-manifest.f87c78f771ca45e78d4e77ede7907283.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8a796fe720da3fc14a9c4289837f04",
+    "url": "/megakheir/index.html"
+  },
+  {
+    "revision": "4a771e7704c004f1fe24",
+    "url": "/megakheir/static/css/main.95cad3e5.chunk.css"
+  },
+  {
+    "revision": "b8ad14c6db314ab6c0cb",
+    "url": "/megakheir/static/js/2.df6f2272.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/megakheir/static/js/2.df6f2272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a771e7704c004f1fe24",
+    "url": "/megakheir/static/js/main.de23aea1.chunk.js"
+  },
+  {
+    "revision": "f3363b397bdc921d073a",
+    "url": "/megakheir/static/js/runtime-main.05f6adc0.js"
+  },
+  {
+    "revision": "967c61a521e91cfe000f9581f176a700",
+    "url": "/megakheir/static/media/Facebook.967c61a5.svg"
+  },
+  {
+    "revision": "0ed5f72ae24902df7118df9f9a3d621e",
+    "url": "/megakheir/static/media/MEGAKHEIR-Logo 1.0ed5f72a.svg"
+  },
+  {
+    "revision": "9c26d8b5706e0db25e664f592384fb4e",
+    "url": "/megakheir/static/media/Powered-by.9c26d8b5.svg"
+  },
+  {
+    "revision": "b1ddbca642e8624733e80a4018fd43b0",
+    "url": "/megakheir/static/media/Vector.b1ddbca6.svg"
+  },
+  {
+    "revision": "12cfaa8c80772948e9065837b8abf03d",
+    "url": "/megakheir/static/media/instagram.12cfaa8c.svg"
+  }
+]);
