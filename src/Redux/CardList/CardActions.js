950ea7efec5b52163340a0897256504daa8cdc34
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-10-02 04:22:08 +0200

    updatesessjkk

--- src/Redux/CardList/CardActions.js
+++ src/Redux/CardList/CardActions.js
@@ -31,16 +31,13 @@ const fetchCardsFailure = (error) => {
 export const fetchCards = () => {
   return (dispatch) => {
     dispatch(fetchCardsRequest);
-    // const configAxios = {
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // };
-
-  
-    
+    const configAxios = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
     axios
-      .get("http://62.68.253.183:8000/ngos/"  )
+      .get("http://62.68.253.183:8000/ngos/" , configAxios )
       .then((res) => {
         console.log(res.data.data,"cardss");
         const cards = res.data.data;
